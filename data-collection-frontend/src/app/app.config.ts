e178535a4d319ed59e8da3647aa5ba97e9104286
Jhohan Capador <jhohancapador@gmail.com>
2026-06-01 20:27:41 -0500

    fix scroll register

--- data-collection-frontend/src/app/app.config.ts
+++ data-collection-frontend/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter, withViewTransitions, withEnabledBlockingInitialNavigation } from '@angular/router';
+import { provideRouter, withInMemoryScrolling, withViewTransitions, withEnabledBlockingInitialNavigation } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
@@ -8,7 +8,15 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes, withViewTransitions(), withEnabledBlockingInitialNavigation()),
+    provideRouter(
+      routes,
+      withViewTransitions(),
+      withEnabledBlockingInitialNavigation(),
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top',
+        anchorScrolling: 'disabled',
+      })
+    ),
     provideHttpClient(),
     provideAnimationsAsync(),
   ]
